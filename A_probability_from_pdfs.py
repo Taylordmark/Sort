6e4655e4d52f7f19920cfde36e7f35d6a3959d0d
Taylor <taylordmark@gmail.com>
2023-12-08 18:25:52 -0500

    Type A working

--- A_probability_from_pdfs.py
+++ A_probability_from_pdfs.py
@@ -2,21 +2,16 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 from scipy.stats import beta
+import pickle
+import os
 
-def read_data(file_path):
-    # Read data from the specified text file
-    object_type_distributions = []
-    with open(file_path, 'r') as file:
-        for line in file:
-            # Parse the line to extract relevant information
-            frame_data = eval(line.strip())
-            
-            # Extract class probabilities and convert them to a numpy array
-            object_features = np.array(frame_data["correct_prob"])
-            
-            # Append the numpy array to the list
-            object_type_distributions.append(object_features)
-    return object_type_distributions
+# Define the path for the parsed dictionary of objects found in frame
+dict_path = 'C:/Users/keela/Documents/Sort/parsed_data_dict.pkl'
+
+# Load data from a pickle file
+with open(dict_path, 'rb') as pickle_file:
+    loaded_frames_detections = pickle.load(pickle_file)
+
 
 def generate_plot(num_classes, object_type_distributions):
     # Define the number of rows (i) and columns (j) for the panel
@@ -34,7 +29,6 @@ def generate_plot(num_classes, object_type_distributions):
 
             # Set the x-axis limits to match the maximum value
             axes[row, col].set_xlim(0, max_value)
-
 
     # Display the histograms
     plt.tight_layout()
@@ -79,7 +73,6 @@ class_dictionary = {
     13: "13"
 }
 
-
 # Define the population percentages for each class
 population_counts = {
     0: 501997,
@@ -99,47 +92,195 @@ population_counts = {
 }
 
 # Log transform initial population values
-population_counts = {key: np.log(value) for key, value in population_counts.items()}
+population_counts = {key: int(np.log(value)) for key, value in population_counts.items()}
 
 # Calculate probabilities based on the log-transformed population percentages
 class_probabilities = get_class_probabilities(population_counts)
 
 
-#Reading data from .txt file
+# Load data from a pickle file
+with open(dict_path, 'rb') as pickle_file:
+    loaded_frames_detections = pickle.load(pickle_file)
+    
+not_bayesed_predictions = []
+for f in loaded_frames_detections.values():
+    for d in f['classes']:
+        not_bayesed_predictions.append(d)
+        
+        
+        
+total_classes = len(loaded_frames_detections[0]['probabilities'][0])
 
-# Specify the path to your data file
-data_file_path = '/remote_home/Thesis/Sort/frames_output.txt'
-
-# Read data from the file
-object_type_distributions = read_data(data_file_path)
+global_data = {i: np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0]]) for i in range(13)}
 
 
-# Iterate through each frame and make predictions
-for frame_data in object_type_distributions:
+
+# This part will not exist in the working code but I need to fill
+# the global data with something for the rest of it to compare to
+
+# Checks if any dict values are > 30, returns True or False
+
+def all_values_length_gt_0(data_dict):
+    """
+    Checks if all values in the dictionary are np arrays with no elements between 0 and 30 (exclusive).
+
+    Args:
+        data_dict: A dictionary where values are np arrays.
+
+    Returns:
+        True if all values have no elements > 30, False otherwise.
+    """
+    for value in data_dict.values():
+        if len(value) > 30:
+            return False
+        else:
+            return True
+
+
+# Calculates and returns all global distribution parameters
+def global_parameterize(data_dict):
+    """
+    Recalculates and returns all global distribution parameters.
+
+    Args:
+        data_dict: A dictionary where values are np arrays.
+
+    Returns:
+        A list of lists containing distribution parameters for each column.
+    """
+    # Initialize an empty list of lists to store parameters
     distribution_parameters = []
-    for row in range(len(class_dictionary)):
-        row_parameters = []
-        for col in range(len(class_dictionary)):
-            data = frame_data[:, col]
+
+    # For each class in the data dictionary
+    for k, history in data_dict.items():
+        # Initialize an empty list to store parameters for the current class
+        col_parameters = []
+
+        # Iterate through each column of the data matrix
+        for col in range(len(history[0])):
+            # Extract data for the current column
+            data = history[:, col]
 
             # Calculate distribution statistics
             a, b, loc, scale = beta.fit(data)
-            # Store parameters in a dictionary
-            row_parameters.append([a, b, loc, scale])
-        distribution_parameters.append(row_parameters)
 
-    pdfs = []
-    for row in distribution_parameters:
-        p = 1
-        for index, (a, b, loc, scale) in enumerate(row):
-            probability = beta.pdf(new_distribution[index], a, b, loc=loc, scale=scale)
-            # Add a little so none multiplied by 0
-            probability += 0.001
-            p *= probability
-        pdfs.append(p)
+            # Store parameters for the current column
+            col_parameters.append([a, b, loc, scale])
 
-    # Multiply by population probabilities
-    pdfs = [pdfs[i] * class_probabilities[i] for i in range(len(pdfs))]
+        # Append parameters for the current class to the main list
+        distribution_parameters.append(col_parameters)
 
-    prediction = pdfs.index(max(pdfs))
-    print(f"\nClass prediction for this frame: {class_dictionary[prediction]}")
+    return distribution_parameters
+
+# Recalculates and returns a local distribution parameter
+def local_parameterize(history):
+    """
+    Recalculates and returns distribution parameters for a single history.
+
+    Args:
+        history: A numpy array representing a single history.
+
+    Returns:
+        A list containing distribution parameters for each column.
+    """
+    # Initialize an empty list to store parameters
+    col_parameters = []
+
+    # Iterate through each column of the history
+    for col in range(len(history[0])):
+        # Extract data for the current column
+        data = history[:, col]
+
+        # Calculate distribution statistics
+        a, b, loc, scale = beta.fit(data)
+
+        # Store parameters for the current column
+        col_parameters.append([a, b, loc, scale])
+
+    return col_parameters
+
+
+print("Filling globals")
+
+
+while all_values_length_gt_0(global_data):
+    for frame, result in loaded_frames_detections.items():
+        for box_num, box in enumerate(result['probabilities']):
+            fake_prediction = result['classes'][box_num]
+            global_data[fake_prediction] = np.vstack((global_data[fake_prediction], box))
+
+print("Globals filled")
+
+
+# Make empty lists a list of zeros for now
+for value in loaded_frames_detections.values():
+    if len(value) == 0:
+        value = [0 for i in range(total_classes)]
+
+# Calculate global distribution parameters
+print("Parameterizing")
+distribution_parameters = global_parameterize(global_data)
+print("Parameterized")
+
+# Now back to our regularly scheduled program
+
+all_predictions = []
+# For each frame in the detections dict
+for frame, value in loaded_frames_detections.items():
+    # For each detection in the frame
+    for detection in range(len(value['boxes'])):
+        new_detection = value['probabilities'][detection]
+        
+        
+        # Calculate the pdfs of the new detection using dist parameters
+        pdfs = []
+        for feature_distribution in distribution_parameters:
+            p = 1
+            for index, (a, b, loc, scale) in enumerate(feature_distribution):
+                probability = beta.pdf(new_detection[index], a, b, loc=loc, scale=scale)
+                # Add a little so none multiplied by 0
+                probability += 0.001
+                p *= probability
+            pdfs.append(p)
+
+        # Multiply by population probabilities
+        pdfs = [pdfs[i] * class_probabilities[i] for i in range(len(pdfs))]
+
+        prediction = pdfs.index(max(pdfs))
+        
+        # Add data to where it was predicted to belong
+        global_data[prediction] = np.vstack((global_data[prediction], new_detection))
+        
+        
+        # Recalculate the respective parameters
+        distribution_parameters[prediction] = local_parameterize(global_data[prediction])
+        all_predictions.append(prediction)
+    print(f"Processing frames {round(frame/len(loaded_frames_detections.keys()), 3)}")
+
+
+        
+# Create the figure and axes
+fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+
+# Plot the first histogram
+axs[0].hist(not_bayesed_predictions, bins=20, alpha=0.5, label="Data 1")
+
+# Plot the second histogram
+axs[1].hist(all_predictions, bins=20, alpha=0.5, label="Data 2")
+
+# Add labels and title
+axs[0].set_xlabel("Values")
+axs[0].set_ylabel("Frequency")
+axs[0].set_title("Not Bayesed Distribution")
+axs[1].set_xlabel("Values")
+axs[1].set_ylabel("Frequency")
+axs[1].set_title("Bayesed Distribution")
+
+# Add legend
+plt.legend(loc="upper right")
+
+# Tight layout
+plt.tight_layout()
+
+# Show the plot
+plt.show()
